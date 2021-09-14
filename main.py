4d36bcf166cf5ee73b4c3c629cb2c20c006a992b
DBAN <dban7171@gmail.com>
2021-09-14 10:28:05 +0900

    DB 속 데이터를 불러와 사용자가 원하는 뉴스 데이터 출력 가능, 없을 시에 데이터가 없다고 호출함

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
+from typing import Optional
+
 import pymysql
 import uvicorn
+from pydantic import BaseModel
+
 import sql_auth
 
-from starlette.responses import JSONResponse
-from fastapi import FastAPI, HTTPException
+
+from starlette.responses import JSONResponse, Response
+from fastapi import FastAPI, HTTPException, Form
 from feed_parser import feed
+
 # to get a string like this run:
 # openssl rand -hex 32
 
 app = FastAPI()
 
-sql = sql_auth.app
 
+#------------------Mysql 설정 부분------------------
+sql = sql_auth.app
 conn = pymysql.Connect(
     db=sql['db'],
     host=sql['host'],
@@ -19,37 +26,51 @@ conn = pymysql.Connect(
     password=sql['password'],
     charset=sql['charset'])
 
-s = conn.cursor()
+s = conn.cursor(pymysql.cursors.DictCursor)
+
+search = "SELECT * FROM news_link"
+s.execute(search)
+result = s.fetchall()
+#------------------Mysql 설정 부분------------------
+
+#------------------Class 선언 부분------------------
 
 
-#-------------------------------------------------------------
+
+#------------------Class 선언 부분------------------
+
+
+#-------------------Api 실행 부분-------------------
+
+@app.get("/news_list")
+async def Newslist():
+    newslist_db = "SELECT name,link FROM news_link "
+    s.execute(newslist_db)
+    newslist = s.fetchall()
+    return newslist
+
+
 
 @app.get("/{news_name}")
-async def get_model(news_name: str):
+async def News(news_name: str):
 
-#if는 한 번에
+    for i in result:
 
-    if news_name.lower() == "inven":
-        rss_url = "http://feeds.feedburner.com/inven"
+        a = i['name']
 
-    elif news_name.lower() == "ruliweb":
-        rss_url = "https://bbs.ruliweb.com/news/rss"
+        if news_name.lower() == a:
+            b = iter(i)
+            c = next(b)
 
-    elif news_name.lower() == "gameinsight":
-        rss_url = "http://www.gameinsight.co.kr/rss/allArticle.xml"
-# {
-    # inven : http://feeds.feedburner.com/inven
-    # inven : http://feeds.feedburner.com/inven
-    # dict
-    # }
-    else:
-        raise HTTPException(status_code=404, detail="Item not found")
+            if c.find(a):
+                rss_url = i['link']
+
+            else:
+                raise HTTPException(status_code=404, detail="Notfound Link Please recheck URL")
 
     News = feed(rss_url)
-
     return JSONResponse(News)
-
-#----------------------------------------------------------
+#-------------------Api 실행 부분-------------------
 
 if __name__ == '__main__':
     uvicorn.run(app)
