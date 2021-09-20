7ad1140ee3f72f6a4033c03b8317057ace6d4467
DBAN <ychysj95@gmail.com>
2021-09-20 15:23:30 +0900

    db연결 부분을 class화 시켜서 반복되는 것들을 쉽게 불러오도록 만듦

--- main.py
+++ main.py
@@ -6,55 +6,80 @@ from pydantic import BaseModel
 
 import sql_auth
 
-
-from starlette.responses import JSONResponse, Response
-from fastapi import FastAPI, HTTPException, Form
+from starlette.responses import JSONResponse
+from fastapi import FastAPI, HTTPException
 from feed_parser import feed
-
-# to get a string like this run:
-# openssl rand -hex 32
 
 app = FastAPI()
 
-
 #------------------Mysql 설정 부분------------------
 sql = sql_auth.app
-conn = pymysql.Connect(
-    db=sql['db'],
-    host=sql['host'],
-    user=sql['user'],
-    password=sql['password'],
-    charset=sql['charset'])
 
-s = conn.cursor(pymysql.cursors.DictCursor)
+
+class dbconn:
+
+    # dbconn을 호출하면 맨 처음 나오는 DB연결 기본 함수
+    def __init__(self,dbname:str):
+        self.connect = pymysql.Connect(
+            db=dbname,
+            host=sql['host'],
+            user=sql['user'],
+            password=sql['password'],
+            charset=sql['charset'])
+
+    # DB를 연결해 이용하기 위한 커서 연결 함수
+    def conn_cursor (self):
+        cur = self.connect.cursor(pymysql.cursors.DictCursor)
+        return cur
+
+    # DB를 커밋하는 함수
+    def conn_commit(self):
+        commit = self.connect.commit()
+        return commit
+    # DB를 닫는 함수
+    def conn_close(self):
+        close = self.connect.close()
+        return close
+
+
+
+
+connect_cursor = dbconn('developer')
+develop_cursor = connect_cursor.conn_cursor()
 
 search = "SELECT * FROM news_link"
-s.execute(search)
-result = s.fetchall()
+develop_cursor.execute(search)
+result = develop_cursor.fetchall()
+
 #------------------Mysql 설정 부분------------------
 
 #------------------Class 선언 부분------------------
 class addNews(BaseModel):
     name : Optional[str]
     link : Optional[str]
-
-
 #------------------Class 선언 부분------------------
 
-new_News = []
+
 #-------------------Api 실행 부분-------------------
-
-
-
 @app.get("/news_list")
 async def Newslist():
     newslist_db = "SELECT name,link FROM news_link "
-    s.execute(newslist_db)
-    newslist = s.fetchall()
+    develop_cursor.execute(newslist_db)
+    newslist = develop_cursor.fetchall()
+
+    connect_cursor.conn_commit()
+    connect_cursor.conn_close()
+
     return newslist
 
+
+# db에 새로운 링크 집어 넣는 곳
 @app.post("/addnews")
 async def UpdateNews(addnews : addNews):
+
+    new_News = []
+    # 근데 여기에 rss 링크가 아닌 일반 링크가 들어오면 망치는데 어떡하지?
+    # 이 작업은 어디서 해야될까? feed_parser에서 진행해야 될 것으로 보이긴함
 
     updates =" INSERT INTO news_link VALUES(%s,%s,%s)"
     add_new_Data = dict(addnews)
@@ -63,28 +88,30 @@ async def UpdateNews(addnews : addNews):
     new_News.append(add_new_Data['name'])
     new_News.append(add_new_Data['link'])
 
-    s.execute(updates,new_News)
-    conn.commit()
+    develop_cursor.execute(updates,new_News)
+
+
     return HTTPException(status_code=200, detail="SUCCESS INSERT DATA")
 
 @app.get("/{news_name}")
 async def News(news_name: str):
+
     for i in result:
-
-        a = i['name']
-
-        if news_name.lower() == a:
+        newsNameData = i['name']
+        if news_name.lower() == newsNameData:
             b = iter(i)
             c = next(b)
 
-            if c.find(a):
+            if c.find(newsNameData):
                 rss_url = i['link']
-
             else:
                 raise HTTPException(status_code=404, detail="Notfound Link Please recheck URL")
+
     News = feed(rss_url)
     return JSONResponse(News)
 #-------------------Api 실행 부분-------------------
 
 if __name__ == '__main__':
     uvicorn.run(app)
+
+    # {data: [{}]}
--- sql_auth.py
+++ sql_auth.py
@@ -2,6 +2,5 @@ app ={
     'user' : 'root',
     'password' : 'root',
     'host' : '127.0.0.1',
-    'db' : 'developer',
     'charset' : 'utf8'
 }
