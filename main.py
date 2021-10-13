86b7997a7616151fc4801242bfd59e5437688754
DBAN <dban7171@gmail.com>
2021-10-13 19:07:50 +0900

    try,except,else,finally를 이용해서 오류가 발생 했을 때 코드가 꺼지지 않고 계속 돌아갈 수 있겠끔 만듬

--- main.py
+++ main.py
@@ -59,36 +59,34 @@ for i in news_link_result:
     news_links = i['link']
     news_name = i['name']
     News = feed(news_links)
-
-
     # 현재 이 반복문은 News 리스트 속 존재하는 딕셔너리를 추출하는 구문
     # 추출한 딕셔너리를 datasaver라는 리스트에 하나씩 집어넣어 DB 컬럼에 맞게 집어 넣는다.
     # 현재 DB의 모든 Column은 NOT NULL이 적용되어 있는데 만약 NULL값이 들어오면 오류가 발생함
     # 이러한 NULL값에 대한 방지로 Not null 해제 해놓고 이에 대한 추가 수정 필요
 
-    for data in range(0, len(News)):
-        news_url = "SELECT url FROM newdata"
-        develop_cursor.execute(news_url)
-        check_url = develop_cursor.fetchall()
 
+    for data in range(0, len(News)):
+        # news_url = "SELECT url FROM newdata"
+        # develop_cursor.execute(news_url)
+        # check_url = develop_cursor.fetchall()
 
         dataUpdates = " INSERT INTO newdata VALUES(default,%s,%s,%s,%s,default,%s)"
         NewsData = News[data]
-        for url_check in check_url:
-            if url_check['url'] == NewsData['url']:
-                # HTTPException(status_code=403, detail="Duplicate Data")
-                print('중복 데이터 존재')
-                pass
-            elif url_check['url'] != NewsData['url']:
-                datasaver = []
-                datasaver.append(NewsData['title'])
-                datasaver.append(NewsData['content'])
-                datasaver.append(NewsData['url'])
-                datasaver.append(news_name)
-                datasaver.append(NewsData['date'])
-                develop_cursor.execute(dataUpdates, datasaver)
-            else:
-                print('데이터가 존재 하지 않습니다.')
+        datasaver = []
+        datasaver.append(NewsData['title'])
+        datasaver.append(NewsData['content'])
+        datasaver.append(NewsData['url'])
+        datasaver.append(news_name)
+        datasaver.append(NewsData['date'])
+        try:
+            develop_cursor.execute(dataUpdates, datasaver)
+        except:
+            print('URL Duplicate Reload Data')
+            pass
+        else:
+            print('오류 없다')
+        finally:
+            print('dd')
 
 connect_cursor.conn_commit()
 connect_cursor.conn_close()
