b6a6eb86f6c6a6e5a86ec592f825de49daeadaf1
DBAN <dban7171@gmail.com>
2021-10-30 14:01:45 +0900

    MySQL서버 reconnect 완료 및 데이터베이스 close로 일어난 오류들 수정 완료

--- RestAPI.py
+++ RestAPI.py
@@ -24,10 +24,11 @@ class dbconn:
             user=sql['user'],
             password=sql['password'],
             charset=sql['charset'],
-            port=55222)
+            port = 55222)
 
     # DB를 연결해 이용하기 위한 커서 연결 함수
     def conn_cursor (self):
+        self.connect.ping(reconnect=True)
         cur = self.connect.cursor(pymysql.cursors.DictCursor)
         return cur
 
@@ -44,6 +45,7 @@ connect_cursor = dbconn('News')
 develop_cursor = connect_cursor.conn_cursor()
 
 
+
 #------------------Class 선언 부분------------------
 
 
@@ -52,21 +54,21 @@ develop_cursor = connect_cursor.conn_cursor()
 # 뉴스 리스트 호출
 @app.get("/newslist")
 async def Newslist():
-    news_link_search = "SELECT * FROM news_link"
 
+    news_link_search = "SELECT * FROM news_link"
     develop_cursor.execute(news_link_search)
     newslist = develop_cursor.fetchall()
-
-    connect_cursor.conn_commit()
-    connect_cursor.conn_close()
-
     return newslist
 
 
 # db에 새로운 링크 집어 넣는 곳
 @app.post("/addnews")
 async def UpdateNews(addnews : addNews):
+
     new_News = []
+    # 근데 여기에 rss 링크가 아닌 일반 링크가 들어오면 망치는데 어떡하지?
+    # 이 작업은 어디서 해야될까? feed_parser에서 진행해야 될 것으로 보이긴함
+
     updates =" INSERT INTO news_link VALUES(default,%s,%s)"
     add_new_Data = dict(addnews)
 
@@ -75,7 +77,6 @@ async def UpdateNews(addnews : addNews):
 
     develop_cursor.execute(updates,new_News)
     connect_cursor.conn_commit()
-    connect_cursor.conn_close()
 
     return HTTPException(status_code=200, detail="SUCCESS INSERT DATA")
 
@@ -86,18 +87,19 @@ async def News(newsname: str):
     findDB = "SELECT * FROM newdata WHERE newsname ="+f'\'{newsname}\''
     develop_cursor.execute(findDB)
     SelectNews = develop_cursor.fetchall()
-
     return SelectNews
 
 # 뉴스 날짜별로
-@app.get("/date/{date}")
+@app.get("/news/{newsname}/{date}")
 async def News(date: str):
 
     findDATE = f"SELECT * FROM newdata WHERE DATE(recordtime) =" +f'\'{date}\''
     develop_cursor.execute(findDATE)
-
     SelectNews = develop_cursor.fetchall()
+
     return SelectNews
+
+#주소입력을 잘 못했으면? 어떻게 표현 할건가?
 
 @app.get("/")
 async def main():
