7fa1dddf1d0f38050d14d219d4e03a1086ad4e98
DBAN <dban7171@gmail.com>
2021-11-04 18:33:12 +0900

    SQLAlchemy로 DB 연결부 변경 중

--- sql_auth.py
+++ sql_auth.py
@@ -1,5 +1,5 @@
 
-Env = 'local'
+Env = 'alche'
 
 if Env == 'local':
 
@@ -9,4 +9,13 @@ if Env == 'local':
         'host' : '0.0.0.0',
         'charset' : 'utf8',
         'port' : 55222
+    }
+
+elif Env == 'alche':
+    app = {
+        'name' : 'mysql+pymysql',
+        'user' : 'root',
+        'password': '1234',
+        'host': '127.0.0.1',
+        'db': 'developer'
     }
