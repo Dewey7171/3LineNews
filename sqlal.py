2ee57a6b863a1a91c7c9ecafda3c7bbdb270da14
DBAN <dban7171@gmail.com>
2021-11-08 00:11:01 +0900

    Test SQLAlchemy 테스트 코드 작성 완료 이제 SQLAlchemy 코드를 본 서비스에 적용 시켜볼 예정 현재 테스트 코드 구현 완료 pymysql 보다 데이터 처리 속도가 향상되었다는 것을 체감이 됨

--- sqlal.py
+++ sqlal.py
@@ -1,3 +1,5 @@
+import uvicorn
+from fastapi import FastAPI
 from sqlalchemy import *
 import sqlalchemy as db
 from sqlalchemy.orm import sessionmaker
@@ -8,22 +10,32 @@ import datetime
 Base = declarative_base()
 sql = sql_auth.app
 
-engine = create_engine(sql['name'] + '://' + sql['user']+ ':'+sql['password']+'@'+sql['host']+'/'+sql['db'])
+engine = create_engine(
+    sql['name'] + '://' + sql['user']+ ':'+sql['password']+'@'+sql['host']+'/'+sql['db'])
 
 conn = engine.connect()
 metadata = db.MetaData()
-table = db.Table('news_link',metadata,autoload=True, autoload_with = engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
+app = FastAPI()
 
-a = session.query(table).all()
-print(a)
+@app.get('/a')
+async def sql():
+    table = db.Table('newdata', metadata, autoload=True, autoload_with=engine)
+    a = session.query(table).all()
+    return a
 
-session.commit()
-session.close()
-print(table.columns.keys())
+@app.get('/b')
+async def sql():
+    table1 = db.Table('news_link', metadata, autoload=True, autoload_with=engine)
+    b = session.query(table1).all()
+    return b
+
+if __name__ == '__main__':
+    uvicorn.run(app)
+
 
 # query = db.select([table])
 # result = conn.execute(query)
