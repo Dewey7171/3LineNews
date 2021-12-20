26ef86a25fc0e9eec2c54551a773c5a35fc6d055
DBAN <dban7171@gmail.com>
2021-12-20 22:52:42 +0900

    Feat : Update 추가
    Link나 Name이 실행이 되지 않을 때 수정하기 위한 장치로 기능을 추가함
    
    CRUD 구현은 이정도로 끝내고 다음번에는 예외처리와 버그 잡는 걸 할 예정

--- Model/db_query.py
+++ Model/db_query.py
@@ -1,4 +1,3 @@
-from sqlalchemy import Table
 from Model import db_connection
 from Model import db_class
 
@@ -7,42 +6,54 @@ session = engine.sessionmaker()
 newsdata = db_class.Newsdata
 newslink = db_class.Newslink
 
+# COMMIT
 def db_commit():
-
     return session.commit()
 
+# CLOSE
 def db_close():
-
     return session.close()
 
+# NEWSDATA GET
 def db_newsdata_get():
     result = session.query(newsdata).all()
 
     return result
 
+# NEWSLIST GET
 def db_newslist_get():
     result = session.query(newslink).all()
 
     return result
 
+# NEWSDATA NAME GET
+def db_newsdata_get_news(news):
+    result = session.query(newsdata).filter(newsdata.name == news).all()
+
+    return result
+
+# NEWSDATA DATE GET
+def db_newsdata_get_date(date):
+    result = session.query(newsdata).filter(newsdata.data.comparator['date'] == date).all()
+
+    return result
+
+# NEWSLINK ADD
 def db_newslink_post(addlist):
     add = newslink(name=addlist.name,link=addlist.link)
     result = session.add(add)
 
     return result
 
-def db_newsdata_get_news(news):
-    result = session.query(newsdata).filter(newsdata.name == news).all()
-
-    return result
-
-def db_newsdata_get_date(date):
-    result = session.query(newsdata).filter(newsdata.data.comparator['date'] == date).all()
-
-    return result
-
+# NEWSLINK DELETE
 def db_newsdata_del_all():
     result = session.query(newsdata).delete()
+
+    return result
+
+# NEWSLINK UPDATE
+def db_newslist_patch(patchlist, id):
+    result = session.query(newslink).filter(newslink.id == id).update({'name':patchlist.name,'link':patchlist.link})
 
     return result
 
