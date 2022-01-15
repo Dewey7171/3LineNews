4e43f796b83bd78e6563a2a9f1f8905bf878ba13
DBAN <dban7171@gmail.com>
2022-01-15 18:19:20 +0900

    Chore : 데이터 처리 과정 간소화
    현재 데이터 처리 과정을 간소화 시킴으로써 데이터 처리하는 과정에서 발생하는 반복문을 없애고 처리된 데이터 통짜를 그대로 DB에 넣으면서 생긴 처리속도가 12초에서 0.x초로 줄었다
    
    그렇다면 나는 이제 여기서 이 데이터를 어떻게 가공해서 남들에게 보여줘야될지 그걸 고민하고 적용시키면 되겠다.
    
    그리고 DB가 꺼졌을 때 휘발성인 Redis를 복구하거나 데이터를 좋게 쌓을 수 있는 방법도 알아봐야겠다.

---
+++ Test/test_feed.py
@@ -0,0 +1,25 @@
+from textrankr import TextRank
+from typing import List
+
+import feedparser
+
+#리스트형식으로 저장하기 위한 클래스 선언
+
+class MyTokenizer:
+    def __call__(self, text: str) -> List[str]:
+        tokens: List[str] = text.split()
+        return tokens
+
+# 리스트 형식으로 저장하는 공간을 만들고 그 공간에 데이터들을 요약한다.
+mytokenizer: MyTokenizer = MyTokenizer()
+textrank: TextRank = TextRank(mytokenizer)
+# 요약한 데이터는 3줄까지 가능하도록 설정하는 변수
+k: int = 3
+
+# 데이터 가공 없이 바로 넣으면 데이터 처리 속도가 엄청나게 빠르다.
+
+def feed(rss_url : str) :
+
+    rss_feed = feedparser.parse(rss_url)
+    data = rss_feed.entries
+    return data
--- Test/test_redis.py
+++ Test/test_redis.py
@@ -1,7 +1,7 @@
 import json
 import redis
 
-from Presenter.pre_feed_parser import feed
+from test_feed import feed
 from datetime import date
 # 휘발성이니까 데이터가 날라감
 
@@ -10,10 +10,12 @@ rd = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 data = feed("http://feeds.feedburner.com/inven")
 a = str(date.today())
+b = 'q'
 jsonDataDict = json.dumps(data, ensure_ascii=False).encode('utf-8')
-
-#rd.set(a,jsonDataDict)
-asd = rd.get('2021-12-30')
+#
+rd.set(b,jsonDataDict)
+asd = rd.get('q')
 qwert = json.loads(asd)
 
-print(qwert)
+print(qwert[0]['summary'])
+
