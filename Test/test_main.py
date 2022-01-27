125f0fd0437096061aa291fa7f57c64bd0ed24c8
DBAN99 <dban7171@gmail.com>
2022-01-27 18:32:37 +0900

    Chore : 주석 처리 변경

--- Test/test_main.py
+++ Test/test_main.py
@@ -1,22 +1,22 @@
-# from datetime import date
-# from fastapi import FastAPI
-# from Presenter.pre_feed_parser import feed
-#
-# import json
-# import redis
-#
-#
-#
-# app = FastAPI()
-#
-# rd = redis.StrictRedis(host='localhost', port=6379, db=0)
-#
-#
-# # 뉴스 내용 전체 호출
-# @app.get('/all', tags=["news"])
-# async def get_all():
-#     data = rd.get('q')
-#     result = json.loads(data)
-#
-#
-#     return result
+from datetime import date
+from fastapi import FastAPI
+from Presenter.pre_feed_parser import feed
+
+import json
+import redis
+
+
+
+app = FastAPI()
+
+rd = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+
+# 뉴스 내용 전체 호출
+@app.get('/all', tags=["news"])
+async def get_all():
+    data = rd.get('q')
+    result = json.loads(data)
+
+
+    return result
