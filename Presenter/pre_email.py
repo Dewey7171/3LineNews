80dc5a430c81ae8c8dd3b1b646b1660a7646d10a
DBAN99 <dban7171@gmail.com>
2022-02-10 17:40:59 +0900

    Feat : Email Sending
    Gmail로 메일 보내는 기능 구현

---
+++ Presenter/pre_email.py
@@ -0,0 +1,26 @@
+import smtplib
+
+from email.mime.text import MIMEText
+from Presenter import pre_auth
+
+s = smtplib.SMTP('smtp.gmail.com', 587)
+s.ehlo()
+s.starttls()
+s.login(pre_auth.email['email'], pre_auth.email['code'])
+
+msg = MIMEText('내용 : 본문내용 테스트입니다.')
+
+msg['Subject'] = '제목 : 메일 보내기 테스트입니다.'
+
+
+
+# 메일 보내기
+
+s.sendmail("3Line@news.com", "dban7171@gmail.com", msg.as_string())
+
+# 세션 종료
+
+s.quit()
+
+
+
